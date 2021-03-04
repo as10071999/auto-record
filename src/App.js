83e853b7cb58750f811dc93f64056c799d997f14
Aryan Singh <as10071999@gmail.com>
2021-03-04 16:50:51 +0530

    🚧 making Form for Inserting links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Form, Col, Row, Button, Container } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
+    <Container fluid className="App">
+      <Form>
+        <Form.Group as={Row} controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Meet Link
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="url" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row}>
+          <Col>
+            <Button variant="success">Start</Button>
+          </Col>
+          <Col>
+            <Button variant="danger">End</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
 
